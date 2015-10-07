092472a54bfc37995ad22d0b2f9805f492bd97db
zomato <zomato@zoms-MacBook-Air.local>
2015-10-07 15:39:34 +0530

    Modified with Select Option for Charts

--- test1.py
+++ test1.py
@@ -22,59 +22,96 @@ def addQuery():
 @app.route('/userHome',methods = ['POST','GET'])
 def getQuery():
 	if request.method == 'GET':
-		# Fetch all data from the database test2 and table output -- if we have 1 query
 		try:
-			main_list = []
-			index_list = []
-			conn = mysql.connect()
-			cursor = conn.cursor()
-			cursor.execute("SELECT DISTINCT query_index FROM output;")
-			query_indexes = cursor.fetchall()
-			for index in query_indexes:
-				result_list = []
-				curr_index = int(index[0])
-				sql = "SELECT field1, field2 FROM output WHERE query_index = %d;" % (curr_index)
-				cursor.execute(sql)
-				data = cursor.fetchall()
-				for result in data:
-					result_dict = [int(result[0]),int(result[1])]
-					result_list.append(result_dict)
-				main_list.append(result_list)
-			return render_template('userHome.html', name = main_list, div_id = 0);
+			main_list, chart_list = show_output();
+			return render_template('userHome.html', name = main_list, charts = chart_list);
 		except Exception as e:
 			return json.dumps({'error':str(e)})
-		finally:
-			cursor.close() 
-			conn.close()
-		pass
 	else: 
-		# Fetch data from input query and add that to output table
 		try:
-			order_list = []
-			conn = mysql.connect()
-			cursor = conn.cursor()
-			cursor.execute("UPDATE check_val SET last_val = last_val + 1 WHERE uid = 1;")
-			data2 = cursor.fetchall()
-			sql = request.form['inputQuery']
-			cursor.execute(sql)
-			data = cursor.fetchall()
-			for order in data:
-				order_dict = {
-					'field1' : order[0],
-					'field2' : order[1] }
-				cursor.execute("USE test2")
-				cursor.execute("SELECT last_val FROM check_val;")
-				data2 = cursor.fetchall()
-				query = "INSERT INTO output(field1,field2,query_index) VALUES (%s,%s,%s);" % (order_dict['field1'], order_dict['field2'],int(data2[0][0]))
-				cursor.execute(query)
-				conn.commit()
-				order_list.append(order_dict)
+			user_query();
 			return redirect('/userHome')
 		except Exception as e:
 			return json.dumps({'error':str(e)})
-		finally:
-			cursor.close() 
-			conn.close()
 
+# This function returns data that is used to draw charts
+def show_output():
+	main_list = []
+
+	index_list = []
+	chart_list = []
+	
+	conn = mysql.connect()
+	cursor = conn.cursor()
+
+	cursor.execute("SELECT DISTINCT chart FROM output;")
+	chart_list = cursor.fetchall()
+
+	cursor.execute("SELECT DISTINCT query_index FROM output;")
+	query_indexes = cursor.fetchall()
+
+	for index in query_indexes:
+		outer_list = []
+		result_list = []
+		
+		curr_index = int(index[0])	
+
+		sql = "SELECT DISTINCT chart FROM output WHERE query_index = %d;" % (curr_index)
+		cursor.execute(sql)
+		chart_list = cursor.fetchall()
+
+		sql = "SELECT field1, field2 FROM output WHERE query_index = %d;" % (curr_index)
+		cursor.execute(sql)
+		data = cursor.fetchall()
+		
+		for result in data:
+			result_dict = [int(result[0]),int(result[1])]
+			result_list.append(result_dict)
+
+		outer_list.append(result_list)
+		outer_list.append(str(chart_list[0][0]))
+		main_list.append(outer_list)
+
+	cursor.close() 
+	conn.close()
+
+	return main_list, chart_list
+
+# This function inputs the user query and stores the returned data
+def user_query():
+	order_list = []
+			
+	conn = mysql.connect()
+	cursor = conn.cursor()
+	
+	cursor.execute("UPDATE check_val SET last_val = last_val + 1 WHERE uid = 1;")
+	data2 = cursor.fetchall()
+	
+	chart_type = request.form.get('chartType')
+	
+	sql = request.form['inputQuery']
+	cursor.execute(sql)
+	data = cursor.fetchall()
+
+	for order in data:
+		order_dict = {
+			'field1' : order[0],
+			'field2' : order[1] }
+
+		cursor.execute("SELECT last_val FROM check_val;")
+		data2 = cursor.fetchall()
+		
+		query = "INSERT INTO output(field1,field2,query_index,chart) VALUES (%s,%s,%s,'%s');" % (order_dict['field1'], order_dict['field2'],int(data2[0][0]),str(chart_type))
+		cursor.execute(query)
+		
+		conn.commit()
+		order_list.append(order_dict)
+	
+	print order_list
+
+	cursor.close() 
+	conn.close()
+
+# App Run
 if __name__ == '__main__':
 	app.run(debug=True, port = 5002)
